bfe1784d2a77f6ebe5a2689cf551fca70fb1d66c
Jong-Hyuk, Jung <heretor92@gmail.com>
2014-07-10 15:12:16 -0700

    Added register function with passport pkg.

--- budgetwiser/apps/account/routes.js
+++ budgetwiser/apps/account/routes.js
@@ -1,12 +1,48 @@
-var express = require('express');
+var express = require('express'),
+    passport = require('passport'),
+    accountModels = require('./models');
+
+var User = accountModels.User;
 
 function index(req, res){
-    // index function
+    if(req.session.user){
+        res.redirect('back');
+    }else{
+        res.render('account/login');
+    }
+}
+function login(req, res){
+    
+}
+function register(req, res){
+    var username = req.param('username'),
+        password = req.param('password');
+
+    User.register(
+        new User({
+            username: username,
+            profile: {
+                nickname: username + '_nickname'
+            }
+        }), password, function(err, account) {
+        if (err) {
+            console.error(err);
+            return res.redirect('/login');
+        }else{
+            console.log(account);
+            passport.authenticate('local')(req, res, function(){
+                console.log("logined user " + username);
+                res.redirect('/login');
+            });
+        }
+    });
 }
 
 // routes initialize
 function setup(app){
-    app.get('/', index);
+    app.get('/login', index);
+    app.post('/login', login);
+    app.post('/register', register);
 }
 
 module.exports = setup;
