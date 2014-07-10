43b65a8be4d5c520dad286840dc912702fc8e9c9
Jong-Hyuk, Jung <heretor92@gmail.com>
2014-07-10 11:54:50 -0700

    Set database schema for apps
    + account/models
    + budgetmap/models (just for test)
    + factful/models

--- budgetwiser/apps/account/models.js
+++ budgetwiser/apps/account/models.js
@@ -1,8 +1,18 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
-var fooAccountSchema = new Schema({
-    test: String
+// User model schema
+var userSchema = new Schema({
+    id: {type: String, lowercase: true, trim: true, required: true},
+    password: {type: String, required: true},
+    profile: {
+        username: {type: String, trim: true, required: true},
+        email: {type: String, trim: true},
+        image: {type: String, trim: true, default: "default_profile_image.png"}
+    }
 });
 
-mongoose.model('FooAccount', fooAccountSchema);
+// module exports
+module.exports = {
+    User: mongoose.model('User', userSchema)
+};
