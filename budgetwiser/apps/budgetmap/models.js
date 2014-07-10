dd21a5823f283cd798a69e16510e099708aebaf0
Shavakan <chiyah92@gmail.com>
2014-07-10 12:15:54 -0700

    basic model for budgetmap

--- budgetwiser/apps/budgetmap/models.js
+++ budgetwiser/apps/budgetmap/models.js
@@ -1,14 +1,33 @@
 var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
+    Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
 
-var fooSchema = new Schema({
-    foo: String,
-    boo: {
-        a: String,
-        b: [Number]
-    }
+var levelOneSchema = new Schema({
+    code: Number,
+    name: String
 });
 
+var levelTwoSchema = new Schema({
+    code: Number,
+    name: String,
+    category: ObjectId
+});
+
+var levelThreeSchema = new Schema({
+    name: String,
+    category: ObjectId
+});
+
+var budgetDataSchema = new Schema({
+    budget: Number,
+    year: Number,
+    category: ObjectId
+});
+
+// Export schemas
 module.exports = {
-    Foo: mongoose.model('Foo', fooSchema)
+    Level_1: mongoose.model('LevelOne', levelOneSchema),
+    Level_2: mongoose.model('LevelTwo', levelTwoSchema),
+    Level_3: mongoose.model('LevelThree', levelThreeSchema),
+    BudgetData: mongoose.model('Budget_data', budgetDataSchema)
 };
