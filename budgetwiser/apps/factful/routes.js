3d1d532dbbfdb6d29b81c1acc9482aab44c0f3cb
Jong-Hyuk, Jung <heretor92@gmail.com>
2014-07-10 16:39:27 -0700

    Added article function with constructor

--- budgetwiser/apps/factful/routes.js
+++ budgetwiser/apps/factful/routes.js
@@ -5,9 +5,22 @@ function index(req, res){
     res.send('factful');
 }
 
+function article(req, res){
+    this.index = function(req, res){
+        res.render('factful/article_add', {
+            layout: 'factful/layout'
+        });
+    };
+    this.add = function(req, res){
+    };
+}
+
 // routes initialize
+var article = new article();
+
 function setup(app){
     app.get('/factful/article/list', index);
+    app.get('/factful/article/add', article.index);
 }
 
 module.exports = setup;
