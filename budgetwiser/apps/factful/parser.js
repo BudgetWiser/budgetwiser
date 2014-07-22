8c5dfb67003159af2807bab3f6e7ac28fe6933aa
Jong-Hyuk, Jung <heretor92@gmail.com>
2014-07-22 15:14:37 -0700

    Added trim function and modified something

--- budgetwiser/apps/factful/parser.js
+++ budgetwiser/apps/factful/parser.js
@@ -1,85 +1,84 @@
-function parser(str){
-    function paragraph(str){
-        var p_list = str.split('\n\n');
-        var img_patt = new RegExp('<image http://(.)+>');
+var parser = {};
 
-        p_list.forEach(function(val, i, arr){
-            var type = 0;
+parser.trim = function(str){
+    return String(str).replace(/^\s+|\s+$/g, '');
+};
 
-            if(img_patt.test(val)){
-                arr[i] = val.split('<image ')[1].split('>')[0];
-                type = 1;
-            }
+parser.paragraph = function(str){
+    var p_list = str.split('\n\n');
+    var img_patt = new RegExp('<image http://(.)+>');
 
-            arr[i] = {
-                type: type,
-                content: arr[i]
-            };
-        });
+    p_list.forEach(function(val, i, arr){
+        var type = 0;
 
-        return p_list;
-    }
-    function findMoney(p){
-        var moneyRegex = new RegExp('(([0-9,]+)조( )?)?(([0-9,]+)억( )?)?(([0-9,]+)만( )?)?(여)?원', 'g');
-        var regArray, output = [];
-
-        while((regArray = moneyRegex.exec(p.content)) != null){
-            var money_word = regArray[0],
-                money_1t = regArray[2],
-                money_100m = regArray[5],
-                money_10k = regArray[8];
-
-            if(typeof(money_1t) !== 'undefined'){
-                money_1t = parseInt(money_1t.replace(",", ""));
-                money_1t = money_1t * 1000000000000;
-            }else{
-                money_1t = 0;
-            }
-            if(typeof(money_100m) !== 'undefined'){
-                money_100m = parseInt(money_100m.replace(",", ""));
-                money_100m = money_100m * 100000000;
-            }else{
-                money_100m = 0;
-            }
-            if(typeof(money_10k) !== 'undefined'){
-                money_10k = parseInt(money_10k.replace(",", ""));
-                money_10k = money_10k * 10000;
-            }else{
-                money_10k = 0;
-            }
-
-            if((money_1t + money_100m + money_10k) != 0){
-                output.push({
-                    start: p.content.search(money_word),
-                    end: p.content.search(money_word) + money_word.length,
-                    money: money_1t + money_100m + money_10k
-                });
-            }
-        }//end while
-
-        return output;
-    }
-    function match(str, c){
-        var m_list = [];
-        while(str.search(c) != -1){
-            var m_pos = str.search(c),
-                str = str.slice(m_pos+1);
-
-            if(m_list.length != 0){
-                m_pos = m_list[m_list.length-1] + m_pos + 1;
-            }
-            m_list.push(m_pos);
+        if(img_patt.test(val)){
+            arr[i] = val.split('<image ')[1].split('>')[0];
+            type = 1;
         }
 
-        return m_list;
+        arr[i] = {
+            type: type,
+            content: parser.trim(arr[i])
+        };
+    });
+
+    return p_list;
+};
+
+parser.findMoney = function(p){
+    var moneyRegex = new RegExp('(([0-9,]+)조( )?)?(([0-9,]+)억( )?)?(([0-9,]+)만( )?)?(여)?원', 'g');
+    var regArray, output = [];
+
+    while((regArray = moneyRegex.exec(p.content)) != null){
+        var money_word = regArray[0],
+            money_1t = regArray[2],
+            money_100m = regArray[5],
+            money_10k = regArray[8];
+
+        if(typeof(money_1t) !== 'undefined'){
+            money_1t = parseInt(money_1t.replace(",", ""));
+            money_1t = money_1t * 1000000000000;
+        }else{
+            money_1t = 0;
+        }
+        if(typeof(money_100m) !== 'undefined'){
+            money_100m = parseInt(money_100m.replace(",", ""));
+            money_100m = money_100m * 100000000;
+        }else{
+            money_100m = 0;
+        }
+        if(typeof(money_10k) !== 'undefined'){
+            money_10k = parseInt(money_10k.replace(",", ""));
+            money_10k = money_10k * 10000;
+        }else{
+            money_10k = 0;
+        }
+
+        if((money_1t + money_100m + money_10k) != 0){
+            output.push({
+                start: p.content.search(money_word),
+                end: p.content.search(money_word) + money_word.length,
+                money: money_1t + money_100m + money_10k
+            });
+        }
+    }//end while
+
+    return output;
+};
+
+parser.match = function(str, c){
+    var m_list = [];
+    while(str.search(c) != -1){
+        var m_pos = str.search(c),
+            str = str.slice(m_pos+1);
+
+        if(m_list.length != 0){
+            m_pos = m_list[m_list.length-1] + m_pos + 1;
+        }
+        m_list.push(m_pos);
     }
 
-    return {
-        paragraph: paragraph,
-        findMoney: findMoney
-    };
-}
-
-var parser = parser();
+    return m_list;
+};
 
 module.exports = parser;
